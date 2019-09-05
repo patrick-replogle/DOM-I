2deba5ec70963fcb1c04366dd075dccbc8e0ce41
patrick-replogle <patrickr1138@gmail.com>
2019-09-04 17:27:46 -0700

    mvp is basically done.  might go for stretch.

--- js/index.js
+++ js/index.js
@@ -39,99 +39,64 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 //Header and nav
-let header = document.querySelector('header');
-header.style.display = 'flex';
 
-let nav = document.querySelector('nav');
-let allNav = document.querySelectorAll('nav a');
-allNav[0].textContent = 'Services';
-allNav[1].textContent = 'Product';
-allNav[2].textContent = 'Vision';
-allNav[3].textContent = 'Features';
-allNav[4].textContent = 'About';
-allNav[5].textContent = 'Contact';
+//nav section
+let allA = document.querySelectorAll('a');
+allA[0].textContent = siteContent['nav']["nav-item-1"];
+allA[1].textContent = siteContent['nav']["nav-item-2"];
+allA[2].textContent = siteContent['nav']["nav-item-3"];
+allA[3].textContent = siteContent['nav']["nav-item-4"];
+allA[4].textContent = siteContent['nav']["nav-item-5"];
+allA[5].textContent = siteContent['nav']["nav-item-6"];
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
-
+//color function
+let aColor = allA.forEach((element) => element.style.color = 'green');
+//add a tags
+let nav = document.querySelector('nav');
 let newA1 = document.createElement('a');
-newA1.textContent = 'Stuff';
-nav.prepend(newA1)
+newA1.textContent = "Stuff";
+newA1.style.color = 'green';
+nav.appendChild(newA1);
+
 let newA2 = document.createElement('a');
-newA2.textContent = 'More';
-nav.appendChild(newA2)
+newA2.textContent = "More";
+newA2.style.color = 'green';
+nav.prepend(newA2);
 
-//change color of nav a
-let color = document.querySelectorAll('a')
-color.forEach((element) => element.style.color = 'green');
-
-//CTA content
-
+//cta section and line breaks hard coded
 let cta = document.querySelector('.cta')
 let ctaText = document.querySelector('.cta-text')
-ctaText.style.display = 'flex';
-ctaText.style.flexDirection = 'column';
 let h1 = document.querySelector('.cta-text h1');
 h1.textContent = 'DOM \r\n IS \r\n AWESOME';
 h1.style.whiteSpace = 'pre';
 let button = document.querySelector('button');
-button.textContent = 'Get Started';
 button.style.borderRadius = '5px';
-let circleImage = document.querySelector('.cta #cta-img');
-circleImage.src = 'img/header-img.png';
-circleImage.alt = 'Circle code logo thing';
+button.textContent = siteContent["cta"]["button"];
+let circleImg = document.getElementById("cta-img");
+ circleImg.setAttribute('src', siteContent['cta']['img-src']);
 
-//Main content
+ 
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
-let mainContent = document.querySelector('.main-content');
-mainContent.style.display = 'flex';
-mainContent.style.flexDirection = 'column';
+let allH4 = document.querySelectorAll('h4');
+allH4[0].textContent = siteContent['main-content']["features-h4"];
+allH4[1].textContent = siteContent['main-content']["about-h4"];
+allH4[2].textContent = siteContent['main-content']["services-h4"];
+allH4[3].textContent = siteContent['main-content']["product-h4"];
+allH4[4].textContent = siteContent['main-content']["vision-h4"];
+allH4[5].textContent = siteContent['contact']["contact-h4"];
 
-let topContent = document.querySelector('.top-content');
-topContent.style.display = 'flex';
-let textContent1 = document.querySelector('.text-content');
-textContent1.style.display = 'flex';
-textContent1.style.flexDirection = 'column';
-textContent1.style.justifyContent= 'flex-start';
-textContent1.style.width = '45%';
+let allP = document.querySelectorAll('p');
+allP[0].textContent = siteContent['main-content']["features-content"];
+allP[1].textContent = siteContent['main-content']["about-content"];
+allP[2].textContent = siteContent['main-content']["services-content"];
+allP[3].textContent = siteContent['main-content']["product-content"];
+allP[4].textContent = siteContent['main-content']["vision-content"];
+allP[8].textContent = siteContent['footer']["copyright" ];
 
-let firstH4 = document.querySelector('.top-content .text-content h4');
-firstH4.textContent = 'FEATURES';
-let p1 = document.querySelector('.text-content p');
-p1.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-
-// let textContent2 = document.getElementbyTagName('top-content .text-content .text-content');
-// textContent2.style.display = 'flex';
-// textContent2.style.flexDirection = 'column';
-// textContent2.style.justifyContent= 'flex-start';
-// textContent2.style.width = '45%';
-
-// let secondH4 = document.getElementbyTagName('.top-content .text-content .text-content h4');
-// secondH4.textContent = 'ABOUT';
-// let p2 = document.querySelector('.text-content p');
-// p2.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-
-
-let middleImg = document.querySelector('#middle-img');
-middleImg.src = 'img/mid-page-accent.jpg';
-middleImg.alt = 'code stuffs';
-
-//contact section
-
-let contact = document.querySelector('.contact');
-contact.style.display = 'flex';
-contact.style.flexDirection = 'column';
-contact.style.justifyContent = 'flex-start';
-contact.style.marginBottom = '-60px';
-let contactH4 = document.querySelector('.contact h4');
-contactH4.textContent = 'CONTACT';
+//contact info to get line breaks
 let contactP = document.querySelector('.contact p');
 contactP.textContent = '123 Way 456\r\nStreet Somewhere, USA\r\n \r\n1 (888) 888-8888\r\n \r\nsales@greatidea.io'
 contactP.style.whiteSpace = 'pre';
-
-//footer 
-
-let footer = document.querySelector('footer');
-footer.style.display = 'flex';
-footer.style.justifyContent = 'center';
-let footerP = document.querySelector('footer p');
-footerP.textContent = 'Copyright Great Idea! 2018';
